7ad023374bedda68ab5a8d9b32a24cf0872c8759
Dzenan Jupic <56133904+DzenanJupic@users.noreply.github.com>
2021-04-06 20:06:03 +0200

    Moved UsizeConversions to it's own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![feature(specialization, const_trait_impl)]
 #![allow(incomplete_features)]
 
+pub use crate::counting_sort::CountingSort;
+pub use crate::usize_conversions::UsizeConversions;
+
 // Comparison sorts
 // todo
 pub mod bubble_sort;
@@ -14,9 +17,13 @@ pub mod merge_sort;
 pub mod quick_sort;
 
 // Non-comparison sorts
+#[doc(hidden)]
 pub mod counting_sort;
 // todo
 pub mod radix_sort;
+
+#[doc(hidden)]
+pub mod usize_conversions;
 
 /// A trait for sortable types.
 /// 
@@ -85,51 +92,3 @@ macro_rules! impl_specific_sort {
 impl_specific_sort!(impl Sort for [bool] as counting_sort::CountingSort::sort);
 impl_specific_sort!(impl Sort for [u8] as counting_sort::CountingSort::sort);
 impl_specific_sort!(impl Sort for [i8] as counting_sort::CountingSort::sort);
-
-
-trait UsizeConversions {
-    fn into_usize(self) -> usize;
-    fn as_usize(&self) -> usize;
-
-    fn from_usize(n: usize) -> Self;
-}
-
-macro_rules! impl_usize_conversions {
-    ($ty:ty) => {
-        impl_usize_conversions!($ty => into: {} from: {}); 
-    };
-    ($ty:ty as $($as_usize:tt)*) => { 
-        impl_usize_conversions!($ty => into: { as $($as_usize)*} from: { as $ty}); 
-    };
-    ($ty:ty => into: { $($as_usize:tt)* } from:  { $($from_usize:tt)* }) => {
-        impl const UsizeConversions for $ty {
-            #[inline]
-            fn into_usize(self) -> usize {
-                self $($as_usize)*
-            }
-            
-            #[inline]
-            fn as_usize(&self) -> usize {
-                *self $($as_usize)*
-            }
-            
-            #[inline]
-            fn from_usize(n: usize) -> Self {
-                n $($from_usize)*
-            }
-        }
-    };
-}
-
-impl_usize_conversions!(char => into: { as u8 as usize} from: { as u8 as char });
-
-impl_usize_conversions!(u8 as usize);
-impl_usize_conversions!(i8 as u8 as usize);
-impl_usize_conversions!(u16 as usize);
-impl_usize_conversions!(i16 as u16 as usize);
-impl_usize_conversions!(u32 as usize);
-impl_usize_conversions!(i32 as u32 as usize);
-impl_usize_conversions!(u64 as usize);
-impl_usize_conversions!(i64 as u64 as usize);
-impl_usize_conversions!(usize);
-impl_usize_conversions!(isize as usize);
